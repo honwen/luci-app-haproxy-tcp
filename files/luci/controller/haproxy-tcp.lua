333f370526feec393330c9743fbeb92f449bea55
honwen.chan <honwen.chan@gmail.com>
2017-06-20 22:18:06 +0800

    first commit

---
+++ files/luci/controller/haproxy-tcp.lua
@@ -0,0 +1,4 @@
+module("luci.controller.haproxy-tcp",package.seeall)
+function index()
+entry({"admin","services","haproxy-tcp"},cbi("haproxy-tcp"),_("HAProxy-TCP"),58)
+end
---
+++ files/luci/model/cbi/haproxy-tcp.lua
@@ -0,0 +1,37 @@
+local m, s, o
+
+if luci.sys.call("pgrep -f haproxy-tcp >/dev/null") == 0 then
+	m = Map("haproxy-tcp", translate("HAProxy-TCP"), "%s - %s" %{translate("HAProxy-TCP"), translate("RUNNING")})
+else
+	m = Map("haproxy-tcp", translate("HAProxy-TCP"), "%s - %s" %{translate("HAProxy-TCP"), translate("NOT RUNNING")})
+end
+
+s = m:section(TypedSection, "general", translate("General Setting"))
+s.anonymous   = true
+
+o = s:option(Flag, "enable", translate("Enable"))
+o.rmempty     = false
+
+o = s:option(Value, "listen", translate("Listen Address:Port"))
+o.placeholder = "0.0.0.0:6666"
+o.default     = "0.0.0.0:6666"
+o.rmempty     = false
+
+o = s:option(Value, "timeout", translate("Timeout Connect"))
+o.placeholder = "666"
+o.default     = "666"
+o.datatype    = "range(33, 10000)"
+o.rmempty     = false
+
+o = s:option(Value, "retries", translate("Retries"))
+o.placeholder = "1"
+o.default     = "1"
+o.datatype    = "range(1, 10)"
+o.rmempty     = false
+
+
+o = s:option(DynamicList, "upstreams", translate("UpStream Server"))
+o.placeholder = "8.8.8.8:53"
+o.rmempty     = false
+
+return m
